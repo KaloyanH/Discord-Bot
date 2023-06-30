b1fdbdc5f83b3fda6519459115e8c8fed8ebf206
Калоян Харакчийски <97788449+KaloyanH@users.noreply.github.com>
2023-06-30 14:05:39 +0300

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -11,17 +11,13 @@ authorized_channels = []  # List of the channels you want to authorize for this 
 
 
 @client.event
-async def on_ready():
-    print(f"{client.user.name} successfully connected to server!")
-
-
-@client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     channel = message.channel
     cmd = message.content.lower()
+
     if channel.name in authorized_channels:
         if cmd == "!commands":
             await list_of_commands(channel, message)
@@ -29,17 +25,15 @@ async def on_message(message):
             await hello_command(channel, message)
         elif cmd in ["!bye", "!goodbye"]:
             await bye_command(channel, message)
-        elif cmd.startswith("!weather"):  # Searches for temperature measurements for any city in the world
-            try:
-                location = " ".join(cmd.split(" ", 1)[1:])
-                await get_weather_command(channel, message, location)
-            except IndexError:
-                await channel.send(f"{message.author.mention}, please provide the correct location!")
-        elif cmd.startswith("!cat"):  # Displays a random cat image
+        elif cmd.startswith("!joke"):
+            await get_random_joke(channel)
+        elif cmd.startswith("!weather"):
+            await handle_weather_command(channel, message, cmd)
+        elif cmd.startswith("!cat"):
             await get_a_cat_command(channel, message)
-        elif cmd.startswith("!google"):  # Uses google search engine to search the web
-            query = " ".join(cmd.split(" ", 1)[1:])
-            await get_google_results(channel, message, query)
+        elif cmd.startswith("!google"):
+            await handle_google_search(channel, message, cmd)
+
 
 
 client.run(TOKEN)
