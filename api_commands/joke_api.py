be7155c840604c10c1b0492416f3e29e714ecfd4
Калоян Харакчийски <97788449+KaloyanH@users.noreply.github.com>
2023-06-30 18:15:26 +0300

    Update joke_api.py

--- api_commands/joke_api.py
+++ api_commands/joke_api.py
@@ -5,7 +5,6 @@ async def joke_api_response(channel):
     joke_url = "https://v2.jokeapi.dev/joke/Any"
     response = requests.get(joke_url)
     random_joke = response.json()
-    print(random_joke)
     message = f"{random_joke['setup']}\n{random_joke['delivery']}" if 'setup' in random_joke else random_joke[
         'joke']
     await channel.send(message)
