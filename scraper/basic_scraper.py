de5c1b5e6c41e0c67de597bdb2c7fd38a22e49be
Калоян Харакчийски <97788449+KaloyanH@users.noreply.github.com>
2023-08-24 15:10:12 +0300

    Update basic_scraper.py

--- scraper/basic_scraper.py
+++ scraper/basic_scraper.py
@@ -1,19 +1,17 @@
-import discord
 import pandas as pd
-import matplotlib.pyplot as plt
-from io import BytesIO
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from bs4 import BeautifulSoup
+from scraper.table_visualization import visualize_table
 
 
 def extract_products_from_site(search_term):
-    options = webdriver.EdgeOptions()
+    options = webdriver.ChromeOptions()
     options.add_argument("--headless")
-    driver = webdriver.Edge(options=options)
+    driver = webdriver.Chrome(options=options)
     driver.get(target_url)
 
     search_input = driver.find_element(By.CSS_SELECTOR, ".c-search__input")
@@ -55,31 +53,6 @@ def extract_products_from_site(search_term):
     df = pd.DataFrame(data)
 
     return df
-
-
-async def visualize_table(channel, df):
-    plt.figure(figsize=(12, 8))
-
-    df.sort_values(by='Price', inplace=True, ascending=False)
-
-    bar_width = 0.6
-
-    bars = plt.barh(df['Product Name'], df['Price'], color='skyblue', height=bar_width)
-    plt.ylabel('Product Name')
-    plt.title('Product Prices')
-    plt.tight_layout()
-
-    for index, bar in enumerate(bars):
-        plt.text(bar.get_width(), bar.get_y() + bar.get_height() / 2, df['Price'][index],
-                 ha='left', va='center', rotation=0)
-
-    buffer = BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-
-    await channel.send(file=discord.File(buffer, filename='product_chart.png'))
-
-    plt.close()
 
 
 async def find_product_command(channel, cmd):
